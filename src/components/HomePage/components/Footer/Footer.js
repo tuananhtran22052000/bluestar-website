05ed7319cdf65a50ddf692ee05d5e9d67bc36dc7
Trần Tuấn Anh <tuananhtran22052000@gmail.com>
2023-03-27 23:08:09 +0700

    update code

--- src/components/HomePage/components/Footer/Footer.js
+++ src/components/HomePage/components/Footer/Footer.js
@@ -3,17 +3,20 @@ import classes from './Footer.module.scss'
 import DownApp from 'app//components/DownApp'
 import Image from 'next/image'
 import logoLandscape from '../../../../../image/logoLandscape.png'
+import Link from 'next/link'
 
 const Footer = () => {
   return (
     <div className={classes.container}>
         <div className={classes.rowBetween}>
             <div className={classes.left}>
+            <Link href={'/'}>
                 <Image 
                     src={logoLandscape}
                     alt="img"
                     className={classes.img}
                 />
+                </Link>
                 <p className={classes.text}>Công ty cổ phần BlueStar</p>
                 <p className={classes.text}>Địa chỉ:</p>
                 <p className={classes.text}>© 2022 | Bản quyền thuộc về BlueStar</p>
