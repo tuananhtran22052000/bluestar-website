880f83ed648e3576ffbf16f097f95fc937f24d7c
trinhkhoi <dinhkhoi1@gmail.com>
2023-03-28 15:06:34 +0700

    update content

--- src/components/HomePage/components/Banner/Banner.js
+++ src/components/HomePage/components/Banner/Banner.js
@@ -14,9 +14,10 @@ const Banner = () => {
                 Đầu tư hiệu quả dựa trên giá trị cốt lõi & bền vững
                 </p>
                 <div className={classes.contentText}>
-                    <span className={classes.text}>  Mô tả giá trị này trong giới hạn hiển thị có thể tối đa là 3 dòng.</span><br/>
+                    <span className={classes.text}>  Ứng dụng hỗ trợ bạn ra quyết định đầu tư tốt nhất với  giải pháp BlueStar Rating - ĐỘC QUYỀN và DUY NHẤT tại Việt Nam gồm 6 tiêu chí lớn và 36 tiêu chí con theo tiêu chuẩn quốc tế đã được đăng ký sở hữu trí tuệ.</span>
+                    {/* <br/>
                     <span className={classes.text}> Dưới đây là dòng mô tả thứ 2...</span><br/>
-                    <span className={classes.text}>Đây là dòng mô tả thứ 3</span>
+                    <span className={classes.text}>Đây là dòng mô tả thứ 3</span> */}
                 </div>
                 <DownApp />
             </div>
