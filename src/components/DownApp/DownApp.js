dbc0538c08829fc2b7167ecb1d9288050b61cbe9
Trinh Khoi <khoi@aahcs.org>
2023-04-02 22:45:44 +0700

    update link app store

--- src/components/DownApp/DownApp.js
+++ src/components/DownApp/DownApp.js
@@ -12,11 +12,13 @@ const DownApp = ({customText}) => {
     <div className={classes.container}>
         <p className={clsx(classes.title, customText)}>Tải về ứng dụng</p>
         <div className={classes.content}>
+            <a href='https://apps.apple.com/vn/app/bluestar-investment/id1662455149'>
             <Image 
                 src={appStore}
                 alt="img"
                 className={classes.img}
             />
+            </a>
              <Image 
                 src={googlePlay}
                 alt="img"
