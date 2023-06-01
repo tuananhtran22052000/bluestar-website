6a639c6abb8638e3865ed3f6564fc59d5eede70d
trinhkhoi <dinhkhoi1@gmail.com>
2023-06-01 14:37:41 +0700

    update download button

--- src/commons/PrimaryButton/PrimaryButton.js
+++ src/commons/PrimaryButton/PrimaryButton.js
@@ -2,14 +2,16 @@ import React from 'react'
 import classes from './PrimaryButton.module.scss'
 import clsx from 'clsx'
 
-const PrimaryButton = ({className, classNameTitle, title}) => {
+const PrimaryButton = ({className, classNameTitle, title, onClick}) => {
   return (
-    <div className={clsx(classes.container, className)}>
-        <div className={classes.background} />
-        <p className={clsx(classes.title, classNameTitle)}>
-            {title}
-        </p>
-    </div>
+    <a onClick={onClick}>
+      <div className={clsx(classes.container, className)}>
+          <div className={classes.background} />
+          <p className={clsx(classes.title, classNameTitle)}>
+              {title}
+          </p>
+      </div>
+    </a>
   )
 }
 
